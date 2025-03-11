581e554af8210fed466c41a63ab11c8e4c6a402a
aldanamedina <aldana.medina@udc.es>
2025-03-11 23:14:52 +0100

    2/3 semana_4 hecho + heuristica mejorada

--- ejecutable.cs
+++ ejecutable.cs
@@ -1,8 +1,6 @@
 // Claudia Vidal Otero
 // Aldana Smyna Medina Lostaunau
-using System;
-using System.Collections.Generic;
-using System.Linq;
+
 using Estructuras;
 using Algoritmos;
 
@@ -15,17 +13,17 @@ namespace Ejecutable
     {
         static void Main()
         {
-            List<int> listaReinas = new List<int> {4, 5, 6, 7, 8, 9, 10, 11}; // Diferentes cantidades de reinas a probar
+            List<int> listaReinas = new List<int> {4, 5, 6, 7, 8, 9, 10}; // Diferentes cantidades de reinas a probar
 
             foreach (int reinas in listaReinas)
             {
                 Console.WriteLine($"\nResolviendo para {reinas} reinas...");
 
-                PilaCandidatos cola = new PilaCandidatos();     // Estructura de datos para ejemplo
+                ColaDePrioridad cola = new ColaDePrioridad();     // Estructura de datos para ejemplo
 
                 List<(int, int)> solucionInicial = new List<(int, int)>(); // Solución inicial vacía
 
-                BusquedaProfundidad busqueda1 = new BusquedaProfundidad(cola);  // Insatancia de la clase de búsqueda para el ejemplo
+                BusquedaAvara busqueda1 = new BusquedaAvara(cola);  // Insatancia de la clase de búsqueda para el ejemplo
 
                 var resultado = busqueda1.busqueda(
                     new Solucion(solucionInicial, 0),
@@ -53,19 +51,8 @@ namespace Ejecutable
         /// <param name="solucion">La solución actual.</param>
         /// <param name="nueva_solucion">La nueva solución generada a partir de la actual.</param>
         /// <returns>El coste acumulado hasta la nueva solución.</returns>
-        static int calculo_coste(Solucion solucion, Solucion nueva_solucion)
-        { 
-            return solucion.Coste + 1;
-        }
         
-        /// <summary>
-        /// Calcula la heurística de una solución en el problema de las N reinas.
-        /// La heurística se basa en contar el número de conflictos entre reinas en el tablero.
-        /// </summary>
-        /// <param name="solucion">La solución actual con las coordenadas de las reinas.</param>
-        /// <returns>El número de conflictos entre reinas en la solución dada.</returns>
-
-        static int calculo_heuristica(Solucion solucion)
+        static int contar_conflictos(Solucion solucion)
         {
             int conflictos = 0;
             int n = solucion.Coords.Count;
@@ -84,22 +71,116 @@ namespace Ejecutable
                     }
                 }
             }
-            return conflictos; // Devolvemos la cantidad de conflictos como heurística
+            return conflictos; // Devolvemos la cantidad de conflictos 
+        }
+        static int calculo_coste(Solucion solucion, Solucion nueva_solucion)
+        { 
+            return contar_conflictos(nueva_solucion);
+        }
+        /// <summary>
+        /// Calcula la heurística de una solución en el problema de las N reinas.
+        /// La heurística se basa en contar el número de conflictos entre reinas en el tablero.
+        /// </summary>
+        /// <param name="solucion">La solución actual con las coordenadas de las reinas.</param>
+        /// <returns>El número de conflictos entre reinas en la solución dada.</returns>
+        static int calculo_heuristica(Solucion solucion)
+        {
+            int reinas_colocadas = solucion.Coords.Count;
+
+            // Creamos una lista para almacenar el número de ataques por reina
+            List<int> ataquesPorReina = new List<int>();
+
+            // Contamos los ataques para cada reina
+            for (int i = 0; i < reinas_colocadas; i++)
+            {
+                var reina = solucion.Coords[i];
+                int fila = reina.Item1;
+                int col = reina.Item2;
+                int ataques = 0;
+
+                // Contamos los ataques con otras reinas
+                for (int j = 0; j < reinas_colocadas; j++)
+                {
+                    if (i == j) continue; // No compararnos con la misma reina
+                    var otraReina = solucion.Coords[j];
+                    int filaOtra = otraReina.Item1;
+                    int colOtra = otraReina.Item2;
+
+                    // Verificamos si se atacan: misma fila, columna o diagonal
+                    if (fila == filaOtra || col == colOtra || Math.Abs(fila - filaOtra) == Math.Abs(col - colOtra))
+                    {
+                        ataques++;
+                    }
+                }
+
+                // Guardamos el número de ataques para esta reina
+                ataquesPorReina.Add(ataques);
+            }
+
+            // Ahora buscamos la reina con más ataques
+            int reinaConMasAtaques = ataquesPorReina.IndexOf(ataquesPorReina.Max());
+            var reinaElegida = solucion.Coords[reinaConMasAtaques];
+            int filaReina = reinaElegida.Item1;
+            int columnaReina = reinaElegida.Item2;
+
+            // Buscamos en la misma columna, pero en las filas donde hay menos ataques
+            int mejorColumna = columnaReina;
+            int mejorAtaques = int.MaxValue;
+            int mejorFila = -1;
+
+            // Recorremos todas las posibles posiciones en la misma columna
+            for (int filaPosible = 0; filaPosible < reinas_colocadas; filaPosible++)
+            {
+                // Si la posición no tiene ataques, la consideramos
+                int ataquesPosicion = 0;
+
+                for (int j = 0; j < reinas_colocadas; j++)
+                {
+                    if (j == reinaConMasAtaques) continue;
+                    var otraReina = solucion.Coords[j];
+                    int filaOtra = otraReina.Item1;
+                    int colOtra = otraReina.Item2;
+
+                    if (filaPosible == filaOtra || mejorColumna == colOtra || Math.Abs(filaPosible - filaOtra) == Math.Abs(mejorColumna - colOtra))
+                    {
+                        ataquesPosicion++;
+                    }
+                }
+
+                // Si encontramos una posición con menos ataques, la elegimos
+                if (ataquesPosicion < mejorAtaques)
+                {
+                    mejorAtaques = ataquesPosicion;
+                    mejorFila = filaPosible;
+                }
+            }
+
+            // Si encontramos una nueva posición válida, la movemos
+            if (mejorFila != -1 && mejorAtaques < ataquesPorReina[reinaConMasAtaques])
+            {
+                // Actualizamos la solución si encontramos una mejora
+                solucion.Coords[reinaConMasAtaques] = (mejorFila, mejorColumna);
+                return mejorAtaques; // La heurística ahora es el número de ataques restantes
+            }
+
+            // Si no encontramos ninguna mejora, mantenemos la heurística original
+            return ataquesPorReina.Sum();
         }
 
+
         /// <summary>
-        /// Función que devuelve los vecinos de una solución dada, es decir, las posiciones de las reinas que pueden ser colocadas.
-        /// Genera todas las posibles ubicaciones para la siguiente reina en la siguiente fila.
+        /// Genera los vecinos de una solución dada, asegurándose de que solo se agreguen posiciones válidas.
+        /// Un vecino es una posible ubicación de la siguiente reina en la fila siguiente, sin generar conflictos con las ya colocadas.
         /// </summary>
-        /// <param name="solucion"> La solución actual, que contiene las coordenadas de las reinas colocadas hasta el momento. </param>
-        /// <param name="reinas"> El numero total de reinas que deben ser colocadas. </param>
-        /// <returns> Una lista de coordenadas (tuplas) posibles para la siguiente reina. </returns>
+        /// <param name="solucion">La solución actual con las reinas colocadas hasta el momento.</param>
+        /// <param name="reinas">El número total de reinas a colocar.</param>
+        /// <returns>Una lista de coordenadas (fila, columna) donde se puede colocar la siguiente reina sin generar conflictos.</returns>
         static List<(int, int)> obtener_vecinos(Solucion solucion, int reinas)
         {
             // Si la solución está vacía, comenzamos desde la fila -1
             int row = solucion.Coords.Count == 0 ? -1 : solucion.Coords.Last().Item1;
 
-            List<(int, int)> vecinos = new();
+            List<(int, int)> vecinos = new List<(int, int)>();
 
             // Si aún hay filas disponibles para colocar una reina
             if (row + 1 < reinas)
@@ -107,8 +188,10 @@ namespace Ejecutable
                 // Generamos todas las posibles posiciones en la siguiente fila
                 for (int j = 0; j < reinas; j++)
                 {
+                    // Solo los que sean consistentes 
+                    if (es_consistente(solucion.Coords, row + 1, j))
                     // Agregamos las posibles posiciones en la fila siguiente (row + 1)
-                    vecinos.Add((row + 1, j));
+                        vecinos.Add((row + 1, j));
                 }
             }
             return vecinos;
@@ -146,6 +229,33 @@ namespace Ejecutable
                 }
             }
             return true; // Todas las reinas están colocadas correctamente
+        
+        }
+
+        /// <summary>
+        /// Verifica si colocar una nueva reina en una posición dada es consistente con la solución actual.
+        /// Una solución es consistente si ninguna de las reinas colocadas hasta el momento se ataca entre sí.
+        /// </summary>
+        /// <param name="coords">Lista de coordenadas de las reinas colocadas hasta el momento.</param>
+        /// <param name="nueva_fila">Fila donde se quiere colocar la nueva reina.</param>
+        /// <param name="nueva_columna">Columna donde se quiere colocar la nueva reina.</param>
+        /// <returns>True si la posición es válida y no genera conflictos, False en caso contrario.</returns>
+
+        static bool es_consistente(List<(int, int)> coords, int nueva_fila, int nueva_columna)
+        {
+            for (int i = 0; i < coords.Count; i++)
+            {
+                int fila_existente = coords[i].Item1;
+                int columna_existente = coords[i].Item2;
+
+                // Verifica conflicto en la misma columna o en la misma diagonal
+                if (columna_existente == nueva_columna ||
+                    Math.Abs(fila_existente - nueva_fila) == Math.Abs(columna_existente - nueva_columna))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
